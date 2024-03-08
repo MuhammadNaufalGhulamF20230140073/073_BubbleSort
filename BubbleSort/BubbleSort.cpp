9aa39c67972339edbf27e93e64a0533c8011b75c
MuhammadNaufalGhulamF20230140073 <naufalfakhrudddin@gmail.com>
2024-03-08 15:59:09 +0700

    menambahkan BubbleSortArray

--- BubbleSort/BubbleSort.cpp
+++ BubbleSort/BubbleSort.cpp
@@ -24,9 +24,26 @@ void input(){
 
 	for (int i = 0; i < n; i++) {
 		cout << "Data ke-" << (i + i) << " : ";
-		cin >> a[i];
+		cin >> arr[i];
 
 	}
+
+}
+
+void BubbleSortArray() {
+	int pass = 1;
+	do {
+		for (int j = 0; j <= n - 1 - pass; j++) {
+			if (arr[j] > arr[j + 1]) {
+				int temp;
+				temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+		pass = pass + 1;
+
+	} while (pass <= n - 1);
 
 }
 
